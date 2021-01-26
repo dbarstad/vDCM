3c7ca2a299816d5b86532020c0bdea7cc7286314
dbarstad <dbarstad@barstads.net>
2021-01-25 20:18:47 -0700

    Made dynamic for IP of CIMC

--- Quick_OS_Init.ps1
+++ Quick_OS_Init.ps1
@@ -7,7 +7,9 @@ $ImpPass = Get-Content ./CIMC_Pass
 $password = ConvertTo-SecureString $ImpPass -AsPlainText -Force
 $Imccred = New-Object System.Management.Automation.PSCredential($user,$password)
 
-$handle = Connect-Imc 10.177.250.184 $Imccred
+$CIMC_IP = Read-Host "Enter IP of CIMC ro revert (x.x.x.x/Default - 10.177.250.140)"
+
+$handle = Connect-Imc $CIMC_IP $Imccred
 
 If ( $handle -ne $null ) {
 
