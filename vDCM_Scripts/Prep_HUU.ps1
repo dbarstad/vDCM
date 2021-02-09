d23c7d8f7827a56cace476b50c2680b35e45d5a0
dbarstad <dbarstad@barstads.net>
2021-02-09 10:45:40 -0700

    Restructure v3

--- vDCM_Scripts/Prep_HUU.ps1
+++ vDCM_Scripts/Prep_HUU.ps1
@@ -1,4 +1,4 @@
-# /netboot/vDCM_Scripts
+﻿# /netboot/vDCM_Scripts
 
 Import-module Cisco.imc
 
@@ -6,19 +6,24 @@ $user = "admin"
 $defPass = ConvertTo-SecureString "password" -AsPlainText -Force
 $ImpPass = Get-Content ./CIMC_Pass
 $password = ConvertTo-SecureString $ImpPass -AsPlainText -Force
-$Imccred = New-Object System.Management.Automation.PSCredential($user,$defPass)
+
+# Need to change thie to $defPass for production
+$Imccred = New-Object System.Management.Automation.PSCredential($user,$password)
 
 $DHCP_Hosts = import-csv ./dnsmasq.leases -Header date,status,IP,MAC,hostname
 
 ForEach ($D_Host in $DHCP_Hosts) {
 
-    $handle = Connect-Imc $D_Host.IP $Imccred
-    If ( $handle -ne $null ) {
+    If ( $D_Host.status -eq "add" ) {
 
-    (Get-Content ./NIHUU/multiserver_config) | Select-String -pattern $D_Host.IP -notmatch | Out-File ./NIHUU/multiserver_config
-    Add-Content ./NIHUU/multiserver_config "address=$($D_Host.IP), user=Admin, password=$($ImpPass), imagefile=ucs-c220m5-huu-4.1.1d.iso"
+    (Get-Content ./multiserver_config) | Select-String -pattern $D_Host.IP -notmatch | Out-File ./multiserver_config
+    Add-Content ./multiserver_config "address=$($D_Host.IP), user=Admin, password=$($ImpPass), imagefile=ucs-c220m5-huu-4.1.1d.iso"
     }
     (Get-Content ./dnsmasq.leases) | Select-String -pattern $D_Host.IP -notmatch | Out-File .\dnsmasq.leases
+    Disconnect-Imc
 }
 
 Get-Content ./dnsmasq.leases | ? {$_.trim() -ne "" } | Out-File .\dnsmasq.leases
+
+python2 /netboot/vDCM_Scripts/update_firmware-4.1.2b.py  --configfile=multiserver_config
+grep "ERROR:" update_huu.log
