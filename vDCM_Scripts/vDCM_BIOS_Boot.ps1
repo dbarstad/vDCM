f7ac8895b7b5433b7a1f6b85ea12dbfcae6b02b8
dbarstad <dbarstad@barstads.net>
2021-06-25 09:50:42 -0600

    Update vDCM_BIOS_Boot.ps1
    Updating to force auto on CIMC interface

--- vDCM_Scripts/vDCM_BIOS_Boot.ps1
+++ vDCM_Scripts/vDCM_BIOS_Boot.ps1
@@ -1,4 +1,4 @@
-# /netboot/vDCM_Scripts
+﻿# /netboot/vDCM_Scripts
 
 Import-module Cisco.imc
 
@@ -28,40 +28,39 @@ ForEach ($D_Host in $DHCP_Hosts) {
 
         ForEach ($Serial in $SNArray) {
             If ($Serial.sn -eq $SerialNumber) {
-
+                
                 Write-Host "Serial Number $($SerialNumber) found in sysdata.txt.  Pushing configuration."
                 Get-ImcLsbootDevPrecision | Set-ImcLsbootDevPrecision -ConfiguredBootMode Legacy -Force -RebootOnUpdate No
                 Get-ImcBiosSettings | Set-ImcBiosVfPSata -VpPSata Disabled -Force
                 Get-ImcBiosSettings | Set-ImcBiosVfPackageCStateLimit -VpPackageCStateLimit "No Limit" -Force
                 Get-ImcBiosSettings | Set-ImcBiosVfXPTPrefetch -VpXPTPrefetch "Disabled" -Force
                 Get-ImcBiosSettings | Set-ImcBiosVfEPPProfile -VpEPPProfile "Performance" -Force
-
+                
                 Get-ImcStorageLocalDisk | Set-ImcStorageLocalDisk -AdminAction make-unconfigured-good -Force -Imc $handle
                 Get-ImcStorageVirtualDriveCreatorUsingUnusedPhysicalDrive | Set-ImcStorageVirtualDriveCreatorUsingUnusedPhysicalDrive -AdminState trigger -DriveGroup "[1,2]" -AccessPolicy read-write -CachePolicy direct-io -DiskCachePolicy unchanged -RaidLevel 1 -ReadPolicy no-read-ahead -StripSize 64k -WritePolicy "Write Through" -VirtualDriveName vDCM_boot -Size 227928 -Force
                 Get-ImcStorageVirtualDrive -Id 0 | Set-ImcStorageVirtualDrive -AdminAction set-boot-drive -Force
                 Get-ImcStorageVirtualDrive -Id 0 | Set-ImcStorageVirtualDrive -AdminAction start-fast-initialization -Force
 
                 Get-ImcStorageFlexUtilVirtualDrive | Set-ImcStorageFlexFlashVirtualDrive -AdminAction erase-vd -Force
-
+ 
                 Get-ImcLsbootDevPrecision | Set-ImcLsbootDevPrecision -ConfiguredBootMode Legacy -Force -RebootOnUpdate No
-                Start-ImcTransaction
+                Start-ImcTransaction  
                     Get-ImcLsbootDevPrecision | Add-ImcLsbootHdd -Name "hdd" -Order 1 -Slot "MRAID" -State "Enabled" -Type "LOCALHDD"
                     Get-ImcLsbootDevPrecision | Add-ImcLsbootPxe -Iptype "IPv4" -Name "pxe" -Order 2 -Slot "L" -State "Enabled" -Type "PXE"
                 Complete-ImcTransaction -Force
 #                Get-ImcLocalUser  -AccountStatus "active" | Set-ImcLocalUser -Pwd $ImpPass -Force
                 Get-ImcRackUnit | Set-ImcRackUnit -AdminPower hard-reset-immediate -Force
                 $ChassisInfo = Get-ImcRackUnit
-                Get-ImcMgmtIf | Set-ImcMgmtIf -DhcpEnable No -DnsUsingDhcp No -ExtGw $Serial.cgw -ExtIp $Serial.cip -ExtMask $Serial.cnm -NicMode dedicated -NicRedundancy none -VlanEnable No -Force
-
+                Get-ImcMgmtIf | Set-ImcMgmtIf -AdminDuplex auto -AdminNetSpeed auto -AutoNeg Enabled -DhcpEnable No -DnsUsingDhcp No -ExtGw $Serial.cgw -ExtIp $Serial.cip -ExtMask $Serial.cnm -NicMode dedicated -NicRedundancy none -VlanEnable No -Force
                 $Host_Updated = 1
             }
         }
         If ($Host_Updated) {
-            Write-Host "System $( $ChassisInfo.Serial ) was configured and rebooted via CIMC tools.  PXE installation is underway."
-            Add-Content vDCM_BIOS_Boot.log "System $( $ChassisInfo.Serial ) was configured and rebooted via CIMC tools.  PXE installation is underway."
+            Write-Host "System $( $ChassisInfo.Serial ) with IP $( $Serial.cip ) was configured and rebooted via CIMC tools.  PXE installation is underway."
+            Add-Content vDCM_BIOS_Boot.log "System $( $ChassisInfo.Serial ) with IP $( $Serial.cip ) was configured and rebooted via CIMC tools.  PXE installation is underway."
             $Host_Updated = 0
             }
-        Else {
+        Else {        
             Write-Host "System with IP $( $D_Host.IP ) could not be connected to via CIMC tools."
             Add-Content vDCM_BIOS_Boot.log "System with IP $( $D_Host.IP ) could not be connected to via CIMC tools."
         }
@@ -70,4 +69,4 @@ ForEach ($D_Host in $DHCP_Hosts) {
   }
 }
 
-# (Get-Content -Path update_huu.log) -replace $Log_Search_Text, "CIMC updated" | Out-File update_huu.log
+(Get-Content -Path update_huu.log) -replace $Log_Search_Text, "CIMC updated" | Out-File update_huu.log
